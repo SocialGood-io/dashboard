3dc2253d0c5eea851f65f88863487bd3f0ee6c86
Jesse Orndorff <jesse@jesseorndorff.com>
2016-06-17 12:36:35 +0700

    copy changes

--- client/layouts/Projects.jsx
+++ client/layouts/Projects.jsx
@@ -10,7 +10,9 @@ ProjectsLayout = React.createClass({
 	      			</div>
 					<div className="dashboard">
 				        <h1>Development Innovations Dashboard</h1>
-				        <p className="large">We are helping civil society organizations in Cambodia test, design, and implement technology solutions that solve development challenges. <a href="/">Explore our projects</a> or <a href="http://development-innovations.org/contact-us">get involved with us.</a></p>
+				        <p className="large">The interactive dashboard provides up-to-date information on the projects we've funded to date. At Development Innovations we track our partners' progress through the 5D process. <a href="/">Explore our projects</a> or <a href="http://development-innovations.org/contact-us">get involved with us.</a></p>
+								<p className="large">You can click on each section to see which projects are working in that phase of the process.</p>
+								<p></p>
 						<ProjectPhasesList />
 						<div className="clear"></div>
 						{this.props.content}
